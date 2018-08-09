28d048996bda20644fa9feb499f57398c2993ecc
Sam Byrd <sbyrd@euronetworldwide.com>
2018-08-09 09:28:55 -0500

    initial changes

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import './style.scss';
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                    Movie List
+                </div>`
+    },
+    'movie-filter': {
+      template: `<div id="movie-filter">
+                    Movie Filter
+                </div>`}
+  }
+});
